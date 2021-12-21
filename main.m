d7bf3990206e149e373c0582423a3324cba52730
Gregory John Casamento <greg.casamento@gmail.com>
2021-12-21 12:24:40 -0500

    Change logic to read the workspace.

--- main.m
+++ main.m
@@ -10,6 +10,8 @@
 #import <XCode/PBXCoder.h>
 #import <XCode/PBXContainer.h>
 #import <XCode/NSString+PBXAdditions.h>
+#import <XCode/XCWorkspaceParser.h>
+#import <XCode/XCWorkspace.h>
 
 NSString *
 findProjectFilename(NSArray *projectDirEntries)
@@ -23,6 +25,24 @@ findProjectFilename(NSArray *projectDirEntries)
       if ([[fileName pathExtension] isEqual: @"xcodeproj"] && range.location == NSNotFound)
 	{
 	  return [fileName stringByAppendingPathComponent: @"project.pbxproj"];
+	}
+    }
+
+  return nil;
+}
+
+NSString *
+findWorkspaceFilename(NSArray *projectDirEntries)
+{
+  NSEnumerator *e = [projectDirEntries objectEnumerator];
+  NSString     *fileName;
+
+  while ((fileName = [e nextObject]))
+    {
+      NSRange range = [fileName rangeOfString:@"._"];
+      if ([[fileName pathExtension] isEqual: @"xcworkspace"] && range.location == NSNotFound)
+	{
+	  return [fileName stringByAppendingPathComponent: @"contents.xcworkspacedata"];
 	}
     }
 
@@ -46,69 +66,111 @@ main(int argc, const char *argv[])
   NSString                   *projectDir;
   NSArray                    *projectDirEntries;
   NSFileManager              *fileManager = [NSFileManager defaultManager];
-
+  BOOL                        isProject = NO;
+  const char *arg = NULL;
+  
   projectDir        = [fileManager currentDirectoryPath];
   projectDirEntries = [fileManager directoryContentsAtPath: projectDir];
 
-  // Get the project...
-  if(argv[1] != NULL)
+  if (argc > 1)
     {
-      NSString *xcodeFilePath = [NSString stringWithCString: argv[1]];
-      fileName = [xcodeFilePath
-			 stringByAppendingPathComponent: 
-		     @"project.pbxproj"];
-      if([[xcodeFilePath pathExtension] isEqualToString:@"xcodeproj"] == NO)
-	{
-	  fileName = findProjectFilename(projectDirEntries);
-	  function = [NSString stringWithCString: argv[1]];
-	}
+      arg = argv[1];
+    }
+  else
+    {
+      arg = "build";
+    }
+
+  // Get the project...
+  if(arg != NULL)
+    {
+      NSString *argument = [NSString stringWithCString: arg];
+
+      if ([argument isEqualToString: @"build"] ||
+          [argument isEqualToString: @"install"] ||
+          [argument isEqualToString: @"clean"])
+        {
+          function = argument;
+          fileName = findWorkspaceFilename(projectDirEntries);
+          if (fileName != nil)
+            {
+              isProject = NO;
+            }
+          else
+            {
+              isProject = YES;
+              fileName = findProjectFilename(projectDirEntries);
+            }
+        }
       else
-	{
+        {
+          fileName = [argument stringByAppendingPathComponent: 
+                                 @"project.pbxproj"];
+          if([[argument pathExtension] isEqualToString:@"xcodeproj"] == NO)
+            {
+              fileName = findProjectFilename(projectDirEntries);
+              function = [NSString stringWithCString: argv[1]];
+              isProject = YES;
+            }
+          else  if([[argument pathExtension] isEqualToString:@"xcworkspace"] == NO)
+            {
+              fileName = findWorkspaceFilename(projectDirEntries);
+              function = [NSString stringWithCString: argv[1]];
+              isProject = NO;
+            }
+          
 	  // If there is a project, add the build operation...
 	  if(argv[2] != NULL && argc > 1)
 	    {
 	      function = [NSString stringWithCString: argv[2]];
 	    }
-	}
-
-    }
-  else
-    {
-      fileName = findProjectFilename(projectDirEntries);
+        }
     }
 
   if([function isEqualToString: @""] || function == nil)
     {
       function = @"build"; // default action...
     }
-
+  
   NSString *display = [function stringByCapitalizingFirstCharacter];
-
-  // Unarchive...
-  coder = [[PBXCoder alloc] initWithContentsOfFile: fileName];
-  container = [coder unarchive];
-
-  // Build...
   SEL operation = NSSelectorFromString(function);
-  if ([container respondsToSelector: operation])
-    {
 
-      // build...
-      puts([[NSString stringWithFormat: @"\033[1;32m**\033[0m Start operation %@", display] cString]); 
-      if ([container performSelector: operation])
-	{
-	  puts([[NSString stringWithFormat: @"\033[1;32m**\033[0m %@ Succeeded", display] cString]);
-	}
+  if (isProject)
+    {
+      // Unarchive...
+      coder = [[PBXCoder alloc] initWithContentsOfFile: fileName];
+      container = [coder unarchive];
+      
+      // Build...
+      if ([container respondsToSelector: operation])
+        {        
+          // build...
+          puts([[NSString stringWithFormat: @"\033[1;32m**\033[0m Start operation %@", display] cString]); 
+          if ([container performSelector: operation])
+            {
+              puts([[NSString stringWithFormat: @"\033[1;32m**\033[0m %@ Succeeded", display] cString]);
+            }
+          else
+            {
+              puts([[NSString stringWithFormat: @"\033[1;31m**\033[0m %@ Failed", display] cString]);
+            }
+        }
       else
-	{
-          puts([[NSString stringWithFormat: @"\033[1;31m**\033[0m %@ Failed", display] cString]);
-	}
+        {
+          puts([[NSString stringWithFormat: @"Unknown build operation \"%@",display] cString]);
+        }
     }
   else
     {
-      puts([[NSString stringWithFormat: @"Unknown build operation \"%@",display] cString]);
+      XCWorkspaceParser *p = [XCWorkspaceParser parseWorkspaceFile: fileName];
+      XCWorkspace *w = [p workspace];
+      
+      if ([w respondsToSelector: operation])
+        {
+          [w performSelector: operation];
+        }
     }
-
+  
   // The end...
   [pool release];
 
